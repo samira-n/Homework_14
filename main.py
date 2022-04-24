713ec38dc6eafc8971fa34948cb510ba6e1b19e5
samira-n <s.nabievaaa@gmail.com>
2022-04-24 16:36:51 +0300

    changes

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 from flask import Flask, jsonify
-from utils import search_by_title
-
-app = Flask(__name__)
+import sqlite3
 
 
-@app.route("/movie/<title>")
-def title_page(title):
-    search_title = search_by_title(title)
-    return jsonify(search_title)
+def main():
+
+    app = Flask(__name__)
+
+    def get_db(query):
+        with sqlite3.connect("netflix.db") as connection:
+            cursor = connection.cursor()
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result
+
+    @app.route('/movie/<title>')
+    def get_title(title):
+        query = f"""
+            SELECT title, country, release_year, listed_in AS genre, description, type
+            FROM netflix
+            WHERE title = '{title}'
+            AND type = 'Movie'
+            ORDER BY release_year DESC
+            LIMIT 1
+        """
+        response = get_db(query)[0]
+        response_json = {
+            'title': response[0],
+            'country': response[1],
+            'release_year': response[2],
+            'genre': response[3],
+            'description': response[4],
+            'type': response[5]
+        }
+        return jsonify(response_json)
 
 
-app.run(debug=True)
+
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
-    app.run()
+    main()
